08b1a63a684876a911559c1dac187d73b0cd34e0
Vuizur <Vuizur@users.noreply.github.com>
2024-01-30 17:14:10 +0100

    Add index creation

--- src/main/java/com/pux12/dictionarycreator/service/InsertService.java
+++ src/main/java/com/pux12/dictionarycreator/service/InsertService.java
@@ -31,6 +31,35 @@ public class InsertService {
 
     private static final int BATCH_SIZE = 10000;
 
+    public void createIndexes() {
+        // We create indexes manually because we absolutely don't need them before
+        // inserting data
+        jdbcTemplate.execute("CREATE INDEX etymology_word_idx ON etymology (word);");
+        jdbcTemplate.execute("CREATE INDEX etymology_pos_idx ON etymology (pos);");
+        jdbcTemplate.execute("CREATE INDEX etymology_lang_code_idx ON etymology (lang_code);");
+        jdbcTemplate
+                .execute("CREATE INDEX etymology_source_wiktionary_code_idx ON etymology (source_wiktionary_code);");
+        // translation_code
+        jdbcTemplate.execute("CREATE INDEX translation_code_idx ON translation (code);"); // TODO: might change in
+                                                                                          // future
+        jdbcTemplate.execute("CREATE INDEX translation_word_idx ON translation (word);");
+        // translation_etymology_id_idx
+        jdbcTemplate.execute("CREATE INDEX translation_etymology_id_idx ON translation (etymology_id);");
+        // sense_etymology_id_idx
+        jdbcTemplate.execute("CREATE INDEX sense_etymology_id_idx ON sense (etymology_id);");
+        // sense_examples_sense_id_idx
+        jdbcTemplate.execute("CREATE INDEX sense_examples_sense_id_idx ON sense_examples (sense_id);");
+        // sense_glosses_sense_id_idx
+        jdbcTemplate.execute("CREATE INDEX sense_glosses_sense_id_idx ON sense_glosses (sense_id);");
+        // form_etymology_id_idx
+        jdbcTemplate.execute("CREATE INDEX form_etymology_id_idx ON form (etymology_id);");
+        // form_form_idx
+        jdbcTemplate.execute("CREATE INDEX form_form_idx ON form (form);");
+        // form_form_tags_idx
+        jdbcTemplate.execute("CREATE INDEX form_form_tags_idx ON form_tags (form_id);");
+
+    }
+
     public void insertDataFromWiktionary() {
         ObjectMapper mapper = new ObjectMapper();
         WiktextractDownloader downloader = new WiktextractDownloader();
@@ -219,6 +248,14 @@ public class InsertService {
         } else {
             System.out.println("Inserting data");
             insertDataFromWiktionary();
+            System.out.println("Creating indexes");
+            long startTime = System.currentTimeMillis();
+            createIndexes();
+            long endTime = System.currentTimeMillis();
+            // Print with HH:MM:SS.MS format
+            System.out.println("Creating indexes took " + String.format("%02d:%02d:%02d.%04d",
+                    (endTime - startTime) / 3600000, ((endTime - startTime) / 60000) % 60,
+                    ((endTime - startTime) / 1000) % 60, (endTime - startTime) % 1000));
         }
     }
 }
