cd145c7d8066538b62c315313f6491e663211109
Vuizur <Vuizur@users.noreply.github.com>
2024-02-01 16:51:27 +0100

    Synonym class

---
+++ src/main/java/com/pux12/dictionarycreator/model/Synonym.java
@@ -0,0 +1,56 @@
+package com.pux12.dictionarycreator.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class Synonym {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "etymology_id")
+    @JsonIgnore
+    private Etymology etymology;
+
+    private String word;
+
+    public Synonym(String word) {
+        this.word = word;
+    }
+
+    public Synonym() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Etymology getEtymology() {
+        return etymology;
+    }
+
+    public void setEtymology(Etymology etymology) {
+        this.etymology = etymology;
+    }
+
+    public String getWord() {
+        return word;
+    }
+
+    public void setWord(String word) {
+        this.word = word;
+    }
+}
