86ac12df6616f3db92ebc6025f4b8dc09be4be67
Vuizur <Vuizur@users.noreply.github.com>
2024-02-07 19:13:18 +0000

    More codepoints

--- helper/codepoints.py
+++ helper/codepoints.py
@@ -6,6 +6,10 @@ def show_codepoints(s):
         print(f'{c} U+{ord(c):04X} {unicodedata.name(c)}')
 
 if __name__ == '__main__':
-    STRING = "Serbian"
+    STRING = "дендрохронологиями"
+    STRING2 = "дѐндрохроноло́гиями"
+
     print(f'Codepoints of "{STRING}"')
     show_codepoints(STRING)
+    print(f'Codepoints of "{STRING2}"')
+    show_codepoints(STRING2)
