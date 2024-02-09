4d7a7d40288348081b63a31f1366983f9d8903f7
Vuizur <Vuizur@users.noreply.github.com>
2024-02-09 13:14:18 +0000

    Specify get

--- src/main/java/com/pux12/ultimatedictionaryapi/controller/DictionaryController.java
+++ src/main/java/com/pux12/ultimatedictionaryapi/controller/DictionaryController.java
@@ -10,6 +10,7 @@ import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.pux12.ultimatedictionaryapi.model.entity.Etymology;
 import com.pux12.ultimatedictionaryapi.repository.EtymologyRepository;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -27,27 +28,28 @@ public class DictionaryController {
         return "Greetings from Spring Boot!";
     }
 
-    @RequestMapping("word/{word}")
+    @RequestMapping(value = "word/{word}", method = RequestMethod.GET)
     public ResponseEntity<String> findByWord(@PathVariable String word) {
         return ResponseEntity.ok(etymologyRepository.findByWrd(word));
     }
 
-    @RequestMapping("source/{source_wiktionary_code}")
+    @RequestMapping(value = "source/{source_wiktionary_code}", method = RequestMethod.GET)
     public Page<Etymology> findBySourceWiktionaryCode(@PathVariable String source_wiktionary_code,
             @RequestParam(value = "page", defaultValue = "0") int page,
             @RequestParam(value = "size", defaultValue = "50") int size) {
         return etymologyRepository.findBySourceWiktionaryCode(source_wiktionary_code, PageRequest.of(page, size));
     }
 
-    @RequestMapping("translation/{sourceLangCode}/{targetLangCode}/{word}")
+    @RequestMapping(value = "translation/{sourceLangCode}/{targetLangCode}/{word}", method = RequestMethod.GET)
     public String findTranslation(@PathVariable String sourceLangCode, @PathVariable String targetLangCode,
             @PathVariable String word) {
 
         try {
             ObjectNode result = mapper.createObjectNode();
             String properWiktionaryEntries = etymologyRepository.findProperWiktionaryEntries(sourceLangCode,
-            targetLangCode, word);
-            var translationWithIPA = etymologyRepository.findTranslationWithPosAndIPA(sourceLangCode, targetLangCode, word);
+                    targetLangCode, word);
+            var translationWithIPA = etymologyRepository.findTranslationWithPosAndIPA(sourceLangCode, targetLangCode,
+                    word);
             String contextLessTranslation = etymologyRepository.findContextLessTranslation(sourceLangCode,
                     targetLangCode, word);
 
@@ -67,7 +69,7 @@ public class DictionaryController {
 
     }
 
-    @RequestMapping("propertranslation/{sourceLangCode}/{targetLangCode}/{word}")
+    @RequestMapping(value = "propertranslation/{sourceLangCode}/{targetLangCode}/{word}", method = RequestMethod.GET)
     public String findProperTranslation(@PathVariable String sourceLangCode, @PathVariable String targetLangCode,
             @PathVariable String word) {
         return etymologyRepository.findProperWiktionaryEntries(sourceLangCode, targetLangCode, word);
