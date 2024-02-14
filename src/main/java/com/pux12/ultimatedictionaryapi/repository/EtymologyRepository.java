a6caf7ff0f745bd9122163415111615b64558a9a
Vuizur <Vuizur@users.noreply.github.com>
2024-02-14 17:01:11 +0100

    WIP random word getter

--- src/main/java/com/pux12/ultimatedictionaryapi/repository/EtymologyRepository.java
+++ src/main/java/com/pux12/ultimatedictionaryapi/repository/EtymologyRepository.java
@@ -11,52 +11,53 @@ import com.pux12.ultimatedictionaryapi.model.entity.Etymology;
 
 @Repository
 public interface EtymologyRepository extends JpaRepository<Etymology, Long> {
-  // @EntityGraph(attributePaths = { "senses", "translations", "forms" })
-  // List<Etymology> findByWord(String word);
+
+  public static String AGGREGATE_JSON_ENTRIES = """
+    SELECT json_agg(json_strip_nulls(json_build_object(
+      'id', e.id,
+      'etymology', e.etymology,
+      'lang_code', e.lang_code,
+      'pos', e.pos,
+      'source_wiktionary_code', e.source_wiktionary_code,
+      'word', e.word,
+      'forms', (
+        SELECT json_agg(json_strip_nulls(json_build_object(
+          'id', f.id,
+          'form', f.form,
+          'tags', ft.tags
+        )))
+        FROM form f
+        LEFT JOIN form_tags ft ON f.id = ft.form_id
+        WHERE f.etymology_id = e.id
+      ),
+      'senses', (
+        SELECT json_agg(json_strip_nulls(json_build_object(
+          'id', s.id,
+          'examples', se.examples,
+          'glosses', sg.glosses
+        )))
+        FROM sense s
+        LEFT JOIN sense_examples se ON s.id = se.sense_id
+        LEFT JOIN sense_glosses sg ON s.id = sg.sense_id
+        WHERE s.etymology_id = e.id
+      ),
+      'translations', (
+        SELECT json_agg(json_strip_nulls(json_build_object(
+          'id', t.id,
+          'code', t.lang_code,
+          'lang', t.lang,
+          'sense', t.sense,
+          'word', t.word
+        )))
+        FROM "translation" t
+        WHERE t.etymology_id = e.id
+      )
+    )))
+    FROM etymology e """;
+
+
   // JPA generated queries are simply too slow, so we use native queries instead
-  @Query(value = """
-      SELECT json_agg(json_strip_nulls(json_build_object(
-        'id', e.id,
-        'etymology', e.etymology,
-        'lang_code', e.lang_code,
-        'pos', e.pos,
-        'source_wiktionary_code', e.source_wiktionary_code,
-        'word', e.word,
-        'forms', (
-          SELECT json_agg(json_strip_nulls(json_build_object(
-            'id', f.id,
-            'form', f.form,
-            'tags', ft.tags
-          )))
-          FROM form f
-          LEFT JOIN form_tags ft ON f.id = ft.form_id
-          WHERE f.etymology_id = e.id
-        ),
-        'senses', (
-          SELECT json_agg(json_strip_nulls(json_build_object(
-            'id', s.id,
-            'examples', se.examples,
-            'glosses', sg.glosses
-          )))
-          FROM sense s
-          LEFT JOIN sense_examples se ON s.id = se.sense_id
-          LEFT JOIN sense_glosses sg ON s.id = sg.sense_id
-          WHERE s.etymology_id = e.id
-        ),
-        'translations', (
-          SELECT json_agg(json_strip_nulls(json_build_object(
-            'id', t.id,
-            'code', t.lang_code,
-            'lang', t.lang,
-            'sense', t.sense,
-            'word', t.word
-          )))
-          FROM "translation" t
-          WHERE t.etymology_id = e.id
-        )
-      )))
-      FROM etymology e where e.word = :word
-            """, nativeQuery = true)
+  @Query(value = AGGREGATE_JSON_ENTRIES + "where e.word = :word", nativeQuery = true)
   String findByWrd(@Param("word") String word);
 
   @Query(value = """
@@ -125,7 +126,11 @@ public interface EtymologyRepository extends JpaRepository<Etymology, Long> {
   String findProperWiktionaryEntries(@Param("sourceLangCode") String sourceLangCode,
       @Param("targetLangCode") String targetLangCode, @Param("word") String word);
 
-  // find by source wiktionary code, paginated
+  /* @Query(value = AGGREGATE_JSON_ENTRIES + "where e.lang_code = :langCode and e.source_wiktionary_code = :wiktionaryLangCode and random_number < :randNum order by ")
+  String getRandomWord(@Param("langCode") String langCode, @Param("wiktionaryLangCode") String wiktionaryLangCode, @Param("randNum") float randNum);
+
+ */
+  // find by source wiktionary code, paginated. (Only testing..)
   Page<Etymology> findBySourceWiktionaryCode(String source_wiktionary_code, Pageable pageable);
 
 }
